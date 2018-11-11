9d9c11f129cdb7ff510fc6e652473713f15694f7
Matthias Reischer <matthiasreischer@MacBook-Pro-2.local>
2018-11-11 11:10:40 +0100

    PHP DB Example finished

--- PHP/connect.php
+++ PHP/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_NAME', '');
+define('DB_NAME', 'PHPExample');
 define('HOST_NAME', 'localhost');
 define('PORT', '3306');
-define('USER_NAME', '');
-define('PASSWORD', '');
+define('USER_NAME', 'root');
+define('PASSWORD', 'root');
 ?>
